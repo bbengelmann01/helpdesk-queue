ee510acc9080fe84682665314985da5ca7d37a1e
bbengelmann01 <99991823+bbengelmann01@users.noreply.github.com>
2025-12-04 14:07:29 -0700

    Create CustomerRecord.java

---
+++ CustomerRecord.java
@@ -0,0 +1,13 @@
+public class CustomerRecord {
+	private int arrivalTime, helpTime;
+	public CustomerRecord(int arrivalTime, int helpTime) {
+		this.arrivalTime = arrivalTime;
+		this.helpTime = helpTime;
+	}
+	public int getArrivalTime() {
+		return arrivalTime;
+	}
+	public int getHelpTime() {
+		return helpTime;
+	}
+}
